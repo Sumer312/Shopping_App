b0b4cb3ae6ae966c23a824bc7db01dfb498fe814
Sumer312 <sumergandhi79@gmail.com>
2023-05-09 01:47:32 +0530

    Footer changed

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,43 +1,91 @@
+import { BsFillBasketFill, BsGithub } from "react-icons/bs";
+import { DiNodejsSmall, DiReact, DiMongodb } from "react-icons/di";
+import { GiBearFace } from "react-icons/gi";
+import {
+  SiTailwindcss,
+  SiTypescript,
+  SiExpress,
+  SiDocker,
+  SiVite,
+  SiPostman,
+} from "react-icons/si";
+import { FaGit } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
     <footer className="footer p-10 bg-base-200 text-base-content">
       <div>
-        <svg
-          width="50"
-          height="50"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          fillRule="evenodd"
-          clipRule="evenodd"
-          className="fill-current"
-        >
-          <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-        </svg>
+        <BsFillBasketFill size="3rem" />
         <p>
-          ACME Industries Ltd.
+          Clothing Web-App
           <br />
-          Providing reliable tech since 1992
+          Personal project
         </p>
+        <br />
+        <Link to="https://github.com/Sumer312/ClothingWebApp">
+          <BsGithub size="4rem" />
+        </Link>
+      </div>
+      <ul>
+        <span className="footer-title">Front-End</span>
+        <li className="link link-hover">
+          <DiReact size="1.5rem" /> React
+        </li>
+        <li className="link link-hover">
+          <GiBearFace size="1.5rem" />
+          Zuntand
+        </li>
+        <li className="link link-hover">
+          <SiTailwindcss size="1.5rem" />
+          Tailwind
+        </li>
+      </ul>
+      <div>
+        <span className="footer-title">Back-End</span>
+        <a className="link link-hover">
+          <DiNodejsSmall size="1.5rem" />
+          Node JS
+        </a>
+        <a className="link link-hover">
+          <SiExpress size="1.5rem" />
+          Express
+        </a>
+        <a className="link link-hover">
+          <SiTypescript size="1.5rem" />
+          TypeScript
+        </a>
+        <a className="link link-hover">
+          {" "}
+          <DiMongodb size="1.5rem" />
+          Mongoose(ODM)
+        </a>
       </div>
       <div>
-        <span className="footer-title">Services</span>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
+        <span className="footer-title">Database</span>
+        <a className="link link-hover">
+          <DiMongodb size="1.5rem" />
+          MongoDB
+        </a>
       </div>
       <div>
-        <span className="footer-title">Company</span>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </div>
-      <div>
-        <span className="footer-title">Legal</span>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
+        <span className="footer-title">Tools</span>
+        <a className="link link-hover">
+          <SiDocker size="1.5rem" />
+          Docker
+        </a>
+        <a className="link link-hover">
+          <SiVite size="1.5rem" />
+          Vite
+        </a>
+        <a className="link link-hover">
+          <SiPostman size="1.5rem" />
+          Postman
+        </a>
+        <a className="link link-hover">
+          <FaGit size="1.5rem" />
+          Git
+        </a>
       </div>
     </footer>
   );
